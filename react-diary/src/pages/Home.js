03935fa6bde5ed43ac562a6eba28e7f27d81e8f2
swJaNG12 <cbdjej38483@gmail.com>
2023-10-01 21:13:16 +0900

    Button 컴포넌트 구현

--- react-diary/src/pages/Home.js
+++ react-diary/src/pages/Home.js
@@ -1,4 +1,5 @@
 import { useSearchParams } from 'react-router-dom';
+import Button from '../component/Button';
 
 const Home = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -6,7 +7,23 @@ const Home = () => {
   console.log(searchParams.get('sort'))
 
   return (
-    <div>Home 페이지 입니다.</div>
+    <div>
+      Home
+      <Button 
+        type={'positive'}
+        text={'positive Btn'}
+        onClick={() => console.log('positive Btn')}
+        />
+      <Button 
+        type={'negative'}
+        text={'negative Btn'}
+        onClick={() => console.log('negative Btn')}
+      />
+      <Button 
+        text={'기본 Btn'}
+        onClick={() => console.log('기본 Btn')}
+      />
+    </div>
   );
 }
 
