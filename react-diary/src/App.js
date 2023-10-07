b41fd42920a7380c7013979672675937ac7a8b24
swJaNG12 <cbdjej38483@gmail.com>
2023-10-07 15:32:38 +0900

    일기 데이터 생성, 수정, 삭제 함수 작성

--- react-diary/src/App.js
+++ react-diary/src/App.js
@@ -4,8 +4,61 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
+import { useReducer, useRef } from 'react';
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'CREATE': 
+      return [...state, action.data];
+    case 'UPDATE':
+      return state.map(el => {
+        return el.id === action.data.id ? action.data : el;
+      })
+    case 'DELETE':
+      return state.filter(el => el.id !== action.targetId);
+    default:
+      return state;
+  }
+};
+
 
 function App() {
+  //  data: 일기 state
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  const onCreate = (date, content, emotionId) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef,
+        date: new Date(date).getTime(),
+        content,
+        emotionId
+      }
+    });
+    idRef.current += 1;
+  }
+
+  const onUpdate = (targetId, date, content, emotionId) => {
+    dispatch({
+      type: 'UPDATE',
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotionId
+      }
+    })
+  }
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: 'DELETE',
+      targetId
+    })
+  }
+
   return (
     <div className="App">
       <Routes>
@@ -14,12 +67,6 @@ function App() {
         <Route path='/Diary/:id' element={<Diary />} />
         <Route path='/Edit' element={<Edit />} />
       </Routes>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/New'>New</Link>
-        <Link to='/Diary'>Diary</Link>
-        <Link to='/Edit'>Edit</Link>
-      </div>
     </div>
   );
 }
