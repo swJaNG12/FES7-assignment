70d6282647ebb16466369ad4a10f8f09449409ca
swJaNG12 <cbdjej38483@gmail.com>
2023-10-08 13:50:46 +0900

    정렬 버튼 기능

--- react-diary/src/component/HomeComponent/DiaryList.jsx
+++ react-diary/src/component/HomeComponent/DiaryList.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import Button from '../common/Button/Button';
 import './DiaryList.css';
 
+const sortOptionList = [
+  { value: 'latest', name: '최신순' },
+  { value: 'oldest', name: '오래된 순' }
+];
+
 const DiaryList = ({ data }) => {
+
+  const [sortType, setSortType] = useState('latest');
+  const onChangeSortTYpe = e => {
+    setSortType(e.target.value)
+  }
+
   return(
     <div className='DiaryList'>
       <div className='menu_wrapper'>
         <div className='left_col'>
-          <select></select>
+          <select value={sortType} onChange={onChangeSortTYpe}>
+            {sortOptionList.map((el,idx) => {
+              return <option key={idx} value={el.value}>{el.name}</option>
+            })}
+          </select>
         </div>
         <div className='right_col'>
           <Button text={'새 일기 쓰기'} type={'positive'} />
