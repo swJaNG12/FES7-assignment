a833f70840bbfae991d692c98f0512aab32fc9f1
swJaNG12 <cbdjej38483@gmail.com>
2023-11-21 18:18:55 +0900

    feat : 유저 정보 리스트 렌더링
    - 유저 정보 리스트를 UserInfoList 컴포넌트에 전달
    - UserInfoItem컴포넌트에 각각의 유저 정보를 전달
    - UserInfoList컴포넌트는 ul, UserInfoItem컴포넌트는 li

--- react-project/src/components/UserInfo/UserInfoItem.jsx
+++ react-project/src/components/UserInfo/UserInfoItem.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { styled } from "styled-components";
 
-const UserInfoItem = () => {
-	return <div></div>;
+const StyledLi = styled.li`
+	width: 100%;
+	padding: 6px;
+	border: 1px solid #adadc6;
+`;
+
+const UserInfoItem = ({ user }) => {
+	console.log(user);
+	return (
+		<StyledLi>
+			<p>
+				{user["user-name"]} ({user["user-age"]} years old)
+			</p>
+		</StyledLi>
+	);
 };
 
 export default UserInfoItem;
--- react-project/src/components/UserInfo/UserInfoList.jsx
+++ react-project/src/components/UserInfo/UserInfoList.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import UserInfoItem from "./UserInfoItem";
+import { styled } from "styled-components";
 
-const UserInfoList = () => {
-	return <div></div>;
+const StyledList = styled.ul`
+	width: 600px;
+	margin: 0 auto;
+	margin-top: 20px;
+	padding: 12px 10px;
+	background-color: #fff;
+	border-radius: 10px;
+
+	& li ~ li {
+		margin-top: 6px;
+	}
+`;
+
+const UserInfoList = ({ userInput }) => {
+	return (
+		<StyledList>
+			{userInput.map((user, idx) => {
+				return <UserInfoItem user={user} key={idx} />;
+			})}
+		</StyledList>
+	);
 };
 
 export default UserInfoList;
