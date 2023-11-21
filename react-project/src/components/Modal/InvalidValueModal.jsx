670aaac50da1e1c9d48840e37c6d1487aa3cfd62
swJaNG12 <cbdjej38483@gmail.com>
2023-11-21 18:20:55 +0900

    feat(Modal) : 모달 렌더링및 기본 스타일, 버튼 구현
    - modal 배경, 버튼을 클릭하면 모달이 닫힌다

--- react-project/src/components/Modal/InvalidValueModal.jsx
+++ react-project/src/components/Modal/InvalidValueModal.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { styled } from "styled-components";
 
-const InvalidValueModal = () => {
-	return <div></div>;
+const StyledModalBackground = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: fixed;
+	top: 0;
+	left: 0;
+`;
+
+const StyledInvalidModal = styled.div`
+	width: 300px;
+	background-color: #fff;
+
+	& button {
+		background-color: #672353;
+		color: #fff;
+	}
+`;
+
+const InvalidValueModal = ({ setIsInvalidModal }) => {
+	const modalHandler = () => {
+		setIsInvalidModal(false);
+	};
+
+	return (
+		<StyledModalBackground>
+			<StyledInvalidModal>
+				<p>모달</p>
+				<button onClick={modalHandler}>닫기</button>
+			</StyledInvalidModal>
+		</StyledModalBackground>
+	);
 };
 
 export default InvalidValueModal;
