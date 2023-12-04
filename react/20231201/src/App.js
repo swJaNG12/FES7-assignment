b51beddcda61e8f7f66561370215cef75dc6bc1f
swJaNG12 <cbdjej38483@gmail.com>
2023-12-04 15:26:27 +0900

    useState 를 useReducer로 변경
    useState대신 useReducer를 사용해서 복잡한 형태의 상태를 더 편하게 관리한다.

--- react/20231201/src/App.js
+++ react/20231201/src/App.js
@@ -5,35 +5,35 @@ import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	useEffect(() => {
-		const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
 
-		if (storedUserLoggedInInformation === "1") {
-			setIsLoggedIn(true);
-		}
-	}, []);
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
-	const loginHandler = (email, password) => {
-		localStorage.setItem("isLoggedIn", "1");
-		setIsLoggedIn(true);
-	};
+  const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
 
-	const logoutHandler = () => {
-		localStorage.removeItem("isLoggedIn");
-		setIsLoggedIn(false);
-	};
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
 
-	return (
-		<React.Fragment>
-			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-			<main>
-				{!isLoggedIn && <Login onLogin={loginHandler} />}
-				{isLoggedIn && <Home onLogout={logoutHandler} />}
-			</main>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
